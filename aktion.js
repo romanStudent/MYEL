f8be72d17d89f52a24ad08375d3b51720b2a2577
romanStudent <132225725+romanStudent@users.noreply.github.com>
2023-05-09 14:43:18 +0200

    Add files via upload

---
+++ aktion.js
@@ -0,0 +1,71 @@
+let product_parent = document.querySelector('.products');
+// console.log(product_parent);
+
+class product {
+	constructor(header, img_src, img_alt, price, col) {
+		this.header = header;
+		this.img_src = img_src;
+		this.img_alt = img_alt;
+        this.price = price;
+        this.col = col;
+	}
+
+	render() {
+		let div = document.createElement('div');
+    div.classList.add(`${this.col}`, 'products_item', 'text-center');
+  if(window.innerWidth <= 700) {
+   div.style.marginLeft = "auto";
+   div.style.marginRight = "auto";
+  }
+
+        div.innerHTML = `
+      <h5>${this.header}</h5>
+      <img src="${this.img_src}" alt="${this.img_alt}">
+
+   <div class="col-12 buy flex">
+      <p href="#" class="price col-4">${this.price}</p>
+      <input type="button" name="buy" value="Shop now" class="col-8">
+   </div>
+        `;
+let po = 0;
+
+        product_parent.append(div);
+        console.log(product_parent);
+po++;
+        return div;
+	}
+
+
+}
+
+
+
+let col;
+if(innerWidth >= 700) {
+    col = 'col-4';
+} else {
+   col = 'col-8';
+}
+let product_1 = new product('Stein Necklace in Silver', 'img/Accessories/Stein Necklace.png', 'Stein necklace', '$310', col);
+let product_2 = new product('Gold Ring', 'img/Accessories/image2.png', 'Gold ring', '$350', col);
+let product_3 = new product('Origines Pendart', 'img/Accessories/Origines Pendant.png', 'Pendart', '$280', col);
+
+product_1.render();
+product_2.render();
+product_3.render();
+
+
+let arr = [product_1, product_2, product_3];
+
+
+// for(let i = 0; i < 3; i++) {
+  // arr[i].render();
+// }
+
+console.log(product_parent);
+
+// export let ip = 'wedfr';
+
+let products_slides = product_parent.children;
+console.log(products_slides);
+export default products_slides;
